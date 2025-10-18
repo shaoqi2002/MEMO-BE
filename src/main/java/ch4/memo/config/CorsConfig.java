84d65ceaeab53c7c75070d7fa280e9bbb2f1b8fe
shaoqi <shao_qi@u.nus.edu>
2025-10-18 20:39:11 +0800

    fix: CorsConfig

--- src/main/java/ch4/memo/config/CorsConfig.java
+++ src/main/java/ch4/memo/config/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
                 registry.addMapping("/**")
                         .allowedOrigins(
                             "http://localhost:3000",                  // 本地开发前端
-                            "https://memo-fe.up.railway.app"          // 生产前端域名
+                            "https://memoria.up.railway.app"          // 生产前端域名
                         )
                         .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
                         .allowedHeaders("*")
